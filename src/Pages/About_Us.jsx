2dca6ca7b79770ec45d312a87aec128a75a4eb1a
Mujtaba <72826134+mujtabafromthedarkside@users.noreply.github.com>
2024-05-13 09:00:51 +0500

    About Us page
    - it's responsive. But, there are some very slight alignment issues at some screen sizes (luckily not the desktop or mobile sizes though, somewhere in between) that I don't have the skill to solve.

--- src/Pages/About_Us.jsx
+++ src/Pages/About_Us.jsx
@@ -1,9 +1,73 @@
 // UploadImage.js
 
-import React, { useState } from 'react';
+import React from 'react';
 
 function About_Us() {
-    return (<h1 className="text-3xl font-bold text-green-500 underline text-center">We are a group of 3!</h1> );
+    return (<div className="text-white flex flex-wrap justify-between mx-10">
+        <div className="my-5 flex flex-wrap justify-center">
+            <img
+                src="/assets/hammad.png"
+                alt="Hammad Anwar"
+                className="w-80 h-80"
+            />
+            <div className="flex flex-col">
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Name:</p> 
+                    <p className="">Hammad Anwar</p>
+                </div>
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Interests:</p> 
+                    <p className="">UI/UX Design, AI, Backend Development</p>
+                </div>
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Skills:</p> 
+                    <p className="">C++, Python, Figma, Flask, Azure</p>
+                </div>
+            </div>
+        </div>
+        <div className="my-5 flex flex-wrap justify-center">
+            <img
+                src="/assets/basim.png"
+                alt="Basim Mehmood"
+                className="w-80 h-80"
+            />
+            <div className="flex flex-col">
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Name:</p> 
+                    <p className="">Basim Mehmood</p>
+                </div>
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Interests:</p> 
+                    <p className="">Competitive Programming, AI, Blockchain, Stock Trading</p>
+                </div>
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Skills:</p> 
+                    <p className="">C++, Python, Tensorflow, Solidity</p>
+                </div>
+            </div>
+        </div>
+        <div className="my-5 flex flex-wrap justify-center">
+            <img
+                src="/assets/mujtaba.png"
+                alt="Mujtaba Omar"
+                className="w-80 h-80"
+            />
+            <div className="flex flex-col">
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Name:</p> 
+                    <p className="">Mujtaba Omar</p>
+                </div>
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Interests:</p> 
+                    <p className="">Competitive Programming, AI, Mobile Development</p>
+                </div>
+                <div className="flex">
+                    <p className=" mx-4 w-20 font-bold">Skills:</p> 
+                    <p className="">C++, Python, Flutter</p>
+                </div>
+            </div>
+        </div>
+    </div>);
 }
 
 export default About_Us;
